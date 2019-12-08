91cb62e225a0496db896a0aea658558d126ba29e
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-08 23:40:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
   directors_database
   index = 0
   while index < directors_database.length do
-    pp directors_database
+    pp directors_database[:movies][index][:title]
     ##this is printing the whole database
     #It's a HoAoH
     index += 1
