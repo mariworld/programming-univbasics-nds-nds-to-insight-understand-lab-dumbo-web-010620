65d46f39e0cfb6a2b2382ee677ad524e5508842d
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-09 00:08:05 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ end
 
 def print_first_directors_movie_titles
   index = 0
-  while index < directors_database[0][:movies][index].length do
+  while index < directors_database[0][:movies][index][:title].length do
     pp directors_database[0][:movies][index][:title]
     index += 1
   end
